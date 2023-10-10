6b0184f9c165f7d4c9f28803afe9628f9ff08494
remiPra <remipradere@gmail.com>
2023-10-10 13:58:13 +0200

    function

--- indexs.js
+++ indexs.js
@@ -36,7 +36,6 @@ async function initialize() {
 
 initialize().catch(console.error);
 
-// app.post(`https://beautiful-puppy-1dbb7d.netlify.app/retriever`, async (req, res) => {
 app.post('/retriever', async (req, res) => {
 
     try {
